78df7d19f112f33ff2b3ae3ac0a44a38c352fe82
Haven <Haven.eldebaque@gmail.com>
2021-11-24 18:44:59 +1000

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+# t.string :title
+# t.string :content
+# t.string :user
+# t.string :category
+
+# Could add more:
+# t.integer :temperature
+# t.integer :precipitation
+# t.integer :humidity
+# t.integer :wind
+
+Location.create(title: 'Oreilly', content: 'Oreilly is located on Queenslands border with New South Wales on the elevated Lamington Plateau. The plateau remains heavily vegetated by Gondwana Rainforests.', user: 'Haven', category: 'Light rain showers')
+
+
